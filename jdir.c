61c728212fe1a828d8b4defb5bde3891c90c2c0c
Jrepaal <168350787+Jrepaal@users.noreply.github.com>
2026-05-30 17:37:05 -0500

    Perms working
    More TODOs too

--- jdir.c
+++ jdir.c
@@ -3,29 +3,39 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/stat.h>
 //#include <string.h>
+
+// This code is published under GPL 3.0, see LICENSE for details
 
 /* TODO:                                            -----
 -Columns                                            -----
     -name                                           DONE!
     -filetype                                       DONE!
-    -rwx (global? owner?)
+    -rwx (global? owner?)                           DONE!
     -formatting (random \n after .steam items)      DONE! fixed itself?
 
 -If no args, pwd and check that                     DONE!
 -get d_name, concatinate if too long (15 chars?)    DONE!
 -sudo & error handling for that
+-dynamic column sizing 
+    -find min size (column name)
+    -find longest name, size based on longest
+-flags for 
+    -hidden files, default to .files hidden
+    -size in bytes, kB, mB, gB
 */
 
 
 
 char * chkarg(const char * argp) // need to check if flag too
 {
+    char * cd[PATH_MAX];
     if (argp == NULL)
     {
-        char * cd = getcwd(cd, 0);
+        *cd = getcwd(*cd, sizeof(cd)); 
         //printf("%s", cd);
-        return cd;
+        return *cd;
     } 
     else
     {
@@ -33,6 +43,32 @@ char * chkarg(const char * argp) // need to check if flag too
     }
     
 } 
+
+void mode_to_string(mode_t mode, char *str) {
+
+    // 2. Owner Permissions
+    str[0] = (mode & S_IRUSR) ? 'r' : '-';
+    str[1] = (mode & S_IWUSR) ? 'w' : '-';
+    str[2] = (mode & S_IXUSR) ? 'x' : '-';
+    str[3] = ' ';
+    // 3. Group Permissions
+    str[4] = (mode & S_IRGRP) ? 'r' : '-';
+    str[5] = (mode & S_IWGRP) ? 'w' : '-';
+    str[6] = (mode & S_IXGRP) ? 'x' : '-';
+    str[7] = ' ';
+    // 4. Other Permissions
+    str[8] = (mode & S_IROTH) ? 'r' : '-';
+    str[9] = (mode & S_IWOTH) ? 'w' : '-';
+    str[10] = (mode & S_IXOTH) ? 'x' : '-';
+    str[11] = ' ';
+    // Handle Special Execution Bits (e.g., setuid, setgid, sticky)
+    if (mode & S_ISUID) str[2] = (str[2] == 'x') ? 's' : 'S';
+    if (mode & S_ISGID) str[6] = (str[6] == 'x') ? 's' : 'S';
+    if (mode & S_ISVTX) str[9] = (str[9] == 'x') ? 't' : 'T';
+    
+    // Null-terminate the string
+    str[12] = '\0';
+}
 
 char * checktype(int num) // This should be a header file? One for each system type?? Definied standards somewhere online??? 
 {
@@ -73,6 +109,26 @@ char * checkstr(char * name)
     return str;
 }
 
+char * checkperms (char * name, struct dirent * dir )
+{
+    char * tname = name;
+    struct stat fileStat;
+        // Get file stats
+    if (stat(tname, &fileStat) == -1)
+    {
+        perror("stat");
+        return NULL;
+    }
+
+    stat(tname, &fileStat); // Passes the valid address
+    mode_t mode = fileStat.st_mode;
+
+    unsigned int * usr, * grp, * gbl = malloc(sizeof(unsigned int));
+            
+    char * perms = malloc(13);
+    mode_to_string(mode, perms);
+    return perms;
+}
 
 int main(int argc, const char * argp[]) /* array of args, array of pointers pointing to args; [0] is program name */
 {
@@ -87,8 +143,8 @@ int main(int argc, const char * argp[]) /* array of args, array of pointers poin
     else
     {
         // printf("Filestream Opened!\n");
-        printf("%-15s %s %s\n", "Name", "|", "Type");
-        printf("----------------+----------\n");
+        printf("%-15s %s %-10s %s %-10s\n", "Name", "|", "Type", "|", "Perms (u/g/o)");
+        printf("----------------+------------+---------\n");
     }
 
     DIR *dir; // create directory stream dir, open
@@ -98,17 +154,19 @@ int main(int argc, const char * argp[]) /* array of args, array of pointers poin
 
 
     struct dirent *de;
-
-    char * name; // strings name and type 
+    char * name; // strings name, type, perms
     char * type;
+    char * perms;
 
     while((de = readdir(dir)) != NULL)
     {
         name = checkstr(de->d_name); // by-reference? don't want to print (de->) directly
         type = checktype(de->d_type);
-        printf("%-15s %s %-10s\n", name, "|", type);
+        perms = checkperms(arg, de);
+        printf("%-15s %s %-10s %s %-10s\n", name, "|", type, "|", perms);
     }
     free(name);
+    free(perms);
 
     int exit_state = closedir(dir);
     if(exit_state == 0)
